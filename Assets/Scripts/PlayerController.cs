6185a3a41d1b2ab46da6ea8c09918ec33f6c7c19
caveiramica14 <micaeltiago2006@gmail.com>
2026-03-24 22:54:07 +0000

    Set up tilemap collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,25 @@ using UnityEngine.InputSystem;
 public class PlayerControllerTutorialUpdates : MonoBehaviour
 {
     public InputAction moveAction;
+    Rigidbody2D rigidbody2D;
+    Vector2 move;   
     // Start is called before the first frame update
     void Start()
     {
         moveAction.Enable();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = moveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
-        transform.position = position;
+        move = moveAction.ReadValue<Vector2>();
+        Debug.Log(move);
     }
-
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.fixedDeltaTime;
+        rigidbody2D.MovePosition(position);
+    }
 }
